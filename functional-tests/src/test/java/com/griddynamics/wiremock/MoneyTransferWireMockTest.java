86ca85388c59465359f26bd0635cce856fefa496
asavostytskyi <savostytskyi.anton@gmail.com>
2020-11-30 21:14:12 +0100

    wiremock example test created

---
+++ functional-tests/src/test/java/com/griddynamics/wiremock/MoneyTransferWireMockTest.java
@@ -0,0 +1,43 @@
+package com.griddynamics.wiremock;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.griddynamics.transfer.domain.TransferTransaction;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyTransferWireMockTest extends WireMockExampleBaseTest {
+
+    @Test
+    private void moneyTransferRequestStatusValidation() {
+        final BigDecimal transactionAmount = new BigDecimal("100.0");
+
+        wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo("/currency/convert"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(transactionAmount.toString())));
+
+        Response response = given().baseUri("http://localhost:8083/")
+                .contentType(ContentType.JSON)
+                .when()
+                .body(new TransferTransaction()
+                        .setBankOfRecipient("CB")
+                        .setTransactionAmount(transactionAmount)
+                        .setTransactionCurrency("EUR"))
+                .post("money/transfer");
+
+        BigDecimal sentAmmountPlusCommission = response.jsonPath()
+                .getObject("recipient.total", BigDecimal.class).add(
+                        response.jsonPath()
+                                .getObject("recipient.commission", BigDecimal.class));
+
+        assertThat(sentAmmountPlusCommission)
+                .isEqualTo(transactionAmount);
+    }
+}
--- functional-tests/src/test/java/com/griddynamics/wiremock/WireMockExampleBaseTest.java
+++ functional-tests/src/test/java/com/griddynamics/wiremock/WireMockExampleBaseTest.java
@@ -1,34 +1,28 @@
 package com.griddynamics.wiremock;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.griddynamics.banksinfo.BanksInformationApplication;
-import com.griddynamics.conversion.CurrencyConversionApplication;
 import com.griddynamics.transfer.MoneyTransferApplication;
-import com.griddynamics.transfer.domain.TransferTransaction;
-import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.core.io.support.ResourcePropertySource;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Properties;
-
-import static io.restassured.RestAssured.given;
 
 public class WireMockExampleBaseTest {
 
-    @BeforeClass(alwaysRun = true)
+    protected WireMockServer wireMockServer;
+
+    @BeforeSuite(alwaysRun = true)
     public void servicesStart() throws IOException {
-        final SpringApplication currencyExchange = new SpringApplication(CurrencyConversionApplication.class);
-        final Properties currencyExchangeProps = new Properties();
-        currencyExchangeProps.setProperty("server.port", "8081");
-        currencyExchange.setEnvironment(createTestEnvironment(currencyExchangeProps));
-        currencyExchange.run();
+        wireMockServer = new WireMockServer(WireMockConfiguration.wireMockConfig().port(8888));
+        wireMockServer.start();
 
         final SpringApplication banksInfo = new SpringApplication(BanksInformationApplication.class);
         final Properties bansInfoProps = new Properties();
@@ -40,25 +34,15 @@ public class WireMockExampleBaseTest {
         final SpringApplication moneyTransfer = new SpringApplication(MoneyTransferApplication.class);
         final Properties moneyTransferProps = new Properties();
         moneyTransferProps.setProperty("server.port", "8083");
-        moneyTransferProps.setProperty("currency.conversion.service.url", "http://localhost:8081/");
+        moneyTransferProps.setProperty("currency.conversion.service.url", "http://localhost:8888/");
         moneyTransferProps.setProperty("banks.information.service.url", "http://localhost:8082/");
         moneyTransfer.setEnvironment(createTestEnvironment(moneyTransferProps));
         moneyTransfer.run();
     }
 
-
-    @Test
-    private void moneyTransferRequestStatusValidation() {
-        given().baseUri("http://localhost:8083/")
-                .contentType(ContentType.JSON)
-                .when()
-                .body(new TransferTransaction()
-                        .setBankOfRecipient("CB")
-                        .setTransactionAmount(new BigDecimal("100"))
-                        .setTransactionCurrency("EUR"))
-                .post("money/transfer")
-                .then()
-        .statusCode(HttpStatus.SC_OK);
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() {
+        wireMockServer.stop();
     }
 
     private ConfigurableEnvironment createTestEnvironment(final Properties properties) throws IOException {
